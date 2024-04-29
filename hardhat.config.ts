f211c94246b8812b9fb3784a9cb4c8e74becd4b2
Tyrone Johnson <tjcloa@gmail.com>
2024-04-29 08:01:54 +0300

    add typechain config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -222,4 +222,11 @@ module.exports = {
     mocha: {
       timeout: 1000000,
     },
+    typechain: {
+        outDir: "typechain",
+        target: "ethers-v5",
+        alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+        externalArtifacts: ["external/artifacts"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+        // externalArtifacts: ["external/artifacts/*.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    },
 };
