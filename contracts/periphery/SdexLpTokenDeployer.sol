f5a42716312a9450bc6bb452d9caa46073f41941
cwsnt <undefined@udefined.com>
2024-05-06 20:49:09 +0700

    make beacon address become immutable in deployer

--- contracts/periphery/SdexLpTokenDeployer.sol
+++ contracts/periphery/SdexLpTokenDeployer.sol
@@ -9,7 +9,7 @@ import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
 contract SdexLpTokenDeployer is Ownable {
     using Address for address;
-    address public lpTokenBeaconAddress;
+    address public immutable lpTokenBeaconAddress;
 
     event LpTokenBeaconSet (address indexed oldLpTokenBeaconAddress, address indexed newLpTokenBeaconAddress);
     event LpTokenCreated(address indexed base, address indexed quote, uint256 poolIdx, address sdex, address lpTokenAddress);
@@ -17,13 +17,6 @@ contract SdexLpTokenDeployer is Ownable {
     constructor(address _lpTokenBeaconAddress) {
         require(_lpTokenBeaconAddress.isContract(), "invalid lpTokenBeaconAddress");
         lpTokenBeaconAddress = _lpTokenBeaconAddress;
-    }
-
-    function setLpTokenBeaconAddress(address _newLpTokenBeaconAddress) external onlyOwner {
-        require(_newLpTokenBeaconAddress.isContract(), "invalid _newLpTokenBeaconAddress");
-        emit LpTokenBeaconSet(lpTokenBeaconAddress, _newLpTokenBeaconAddress);
-
-        lpTokenBeaconAddress = _newLpTokenBeaconAddress;
     }
 
     function deployLpToken(address base, address quote, uint256 poolIdx, address sdex) external returns(address) {
